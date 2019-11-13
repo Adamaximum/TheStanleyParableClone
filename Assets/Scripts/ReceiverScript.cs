ba6782b179a1f3a4f9da95a41b5a9ad5b574b64c
the-imitator <25367233+the-imitator@users.noreply.github.com>
2019-11-13 17:16:51 -0500

    Made "ReceiverScriptTemplate.cs"

--- Assets/Scripts/ReceiverScript.cs
+++ Assets/Scripts/ReceiverScript.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 // FUNCTION: This object will run Activate() once the player presses E on it.
 
 // ** NOTE: There is CUSTOM CODE in this script. Whatever object this is placed on, the custom code will be specific to that object!!! 
-public class ReceiverScriptTemplate : MonoBehaviour {
+public class ReceiverScript : MonoBehaviour
+{
     public AudioSource activatedSource;
     //public AudioClip scaryClip;
 
@@ -20,7 +21,8 @@ public class ReceiverScriptTemplate : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         if (isActivated && !played) {
             // [[[[[THIS OBJECT'S CUSTOM CODE STARTS HERE]]]]]
             Debug.Log("Button is Activated. Commence code now!");
