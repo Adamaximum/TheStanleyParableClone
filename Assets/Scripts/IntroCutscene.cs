abf2ce2983b5fccc772085a3cda6f62327a55830
Adam Goren <amg1090@nyu.edu>
2019-12-18 12:03:29 -0500

    Got the intro to work

--- Assets/Scripts/IntroCutscene.cs
+++ Assets/Scripts/IntroCutscene.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class IntroCutscene : MonoBehaviour
 {
     public DialogueManager manager;
     public SpriteRenderer blackout;
+
+    TextMeshProUGUI centerTitle;
+    TextMeshProUGUI clickToSkip;
+
+    public Transform Stanley;
+    public GameObject[] stanleyPose;
 
     public bool fadeIn;
     public int movementPhases;
@@ -16,80 +24,112 @@ public class IntroCutscene : MonoBehaviour
         manager = GameObject.Find("DialogueManager").GetComponent<DialogueManager>();
         blackout = GetComponentInChildren<SpriteRenderer>();
 
-        //StartCoroutine(CameraPath());
+        centerTitle = GameObject.Find("CenterTitle").GetComponent<TextMeshProUGUI>();
+        clickToSkip = GameObject.Find("ClickToSkip").GetComponent<TextMeshProUGUI>();
+
+        Stanley = GameObject.Find("Stanley").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (fadeIn)
-        //{
-        //    blackout.color -= new Color(0, 0, 0, 0.005f);
-        //}
-        //else
-        //{
-        //    blackout.color += new Color(0, 0, 0, 0.005f);
-        //}
+        if (manager.currentLine == 1)
+        {
+            clickToSkip.color += new Color (0f, 0f, 0f, 0.005f);
+        }
+        else if (manager.currentLine == 2)
+        {
+            clickToSkip.color -= new Color(0f, 0f, 0f, 0.005f);
+        }
 
         if (manager.currentLine == 3)
         {
-            
             movementPhases = 1;
         }
 
         if (movementPhases == 1)
         {
+            stanleyPose[0].SetActive(true);
+            stanleyPose[1].SetActive(false);
+
             transform.position -= new Vector3(0.004f, 0f, 0f);
 
-            if (transform.position.x > 6.8f)
+            blackout.color -= new Color(0, 0, 0, 0.005f);
+
+            if (transform.position.x < 6.8f)
             {
-                blackout.color -= new Color(0, 0, 0, 0.01f);
-            }
-            else if (transform.position.x <= 6.8f)
-            {
-                blackout.color += new Color(0, 0, 0, 0.01f);
+                transform.position = new Vector3(3.19f, 1.054f, 1.92f);
+                transform.LookAt(Stanley);
+                movementPhases = 2;
             }
         }
-
-        //else if (manager.currentLine == 4)
-        //{
-        //    if (movementPhases == 0)
-        //    {
-        //        blackout.color += new Color(0, 0, 0, 0.005f);
-        //        if (blackout.color.a == 255)
-        //        {
-        //            movementPhases = 1;
-        //        }
-        //    }
-        //    else if (movementPhases == 1)
-        //    {
-        //        blackout.color -= new Color(0, 0, 0, 0.005f);
-        //    }
-        //}
-
-
-
-
-    }
-
-    //IEnumerator CameraPath()
-    //{
-    //    yield return new WaitForSeconds(5);
-
-    //    fadeIn = true;
-
-    //    yield return new WaitForSeconds(8);
-
-    //    fadeIn = false;
-    //}
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "TriggerNormal")
+        else if (movementPhases == 2)
         {
-            
+            stanleyPose[0].SetActive(false);
+            stanleyPose[1].SetActive(true);
+
+            transform.position -= new Vector3(0f, 0f, 0.004f);
+            transform.LookAt(Stanley);
+
+            if (transform.position.z < -1.7f)
+            {
+                transform.position = new Vector3(0.426f, 2, -0.153f);
+                movementPhases = 3;
+            }
+        }
+        else if (movementPhases == 3)
+        {
+            stanleyPose[0].SetActive(true);
+            stanleyPose[1].SetActive(false);
+
+            transform.position += new Vector3(0.004f, 0f, 0f);
+            transform.LookAt(Stanley);
+
+            if (transform.position.x > 3f)
+            {
+                transform.position = new Vector3(5.841f, 1.1f, 8.1f);
+                transform.localEulerAngles = new Vector3(0, -90f, 0);
+                movementPhases = 4;
+            }
+        }
+        else if (movementPhases == 4)
+        {
+            transform.position += new Vector3(0.0005f, 0f, 0f);
+
+            if (transform.position.x > 6.1f)
+            {
+                blackout.color = new Color(0f, 0f, 0f, 255f);
+            }
+        }
+        else if (movementPhases == 5)
+        {
+            Stanley.position = new Vector3(-1.253f, transform.position.y, -1.491f);
+
+            stanleyPose[0].SetActive(false);
+            stanleyPose[1].SetActive(false);
+
+            blackout.color = new Color(0f, 0f, 0f, 0f);
+            transform.position += new Vector3(0.0005f, 0f, 0f);
+            transform.LookAt(Stanley);
         }
 
-        Debug.Log("Triggered!");
+        if (manager.currentLine == 9)
+        {
+            centerTitle.color += new Color(0f, 0f, 0f, 0.005f);
+        }
+        if (manager.currentLine >= 11)
+        {
+            centerTitle.color -= new Color(0f, 0f, 0f, 0.005f);
+        }
+        if (manager.currentLine == 11 && movementPhases == 4)
+        {
+            transform.position = new Vector3(0.55f, 1.054f, -1.242f);
+            movementPhases = 5;
+        }
+
+        if (manager.currentLine > 15 || Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
